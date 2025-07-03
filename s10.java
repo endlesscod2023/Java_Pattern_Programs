067de5a560f60db9f0995bb5770fe8c5f0f4adc5
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 21:34:44 +0530

    Create s10.java
    create

---
+++ s10.java
@@ -0,0 +1,26 @@
+//Program to print right angled triangle 
+//(print numbers) (ASSIGNMENT)
+class s10
+{
+	public static void main(String[] args) 
+	{
+		for(int j=1;j<=5; j++)
+			{
+			for(int i=1; i<=j; i++ )
+				{
+				System.out.print(i);
+				}
+				System.out.println();
+			}
+	}
+}
+/*
+
+Output
+1
+12
+123
+1234
+12345
+
+*/
