c3f50980ca3e212e25c6f63204fd6ac3f2a2bf63
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 21:32:35 +0530

    Create s9.java
    create

---
+++ s9.java
@@ -0,0 +1,25 @@
+//Program to print right angled triangle.
+class s9 
+{
+	public static void main(String[] args) 
+	{
+		for(int j=1;j<=5; j++)
+			{			
+				for(int i=1; i<=j; i++ )
+					{
+					System.out.print("*");
+					}
+					System.out.println();
+			}
+	}
+}
+/*
+
+Output
+*
+**
+***
+****
+*****
+
+*/
