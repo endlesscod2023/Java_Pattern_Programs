ab677f647d5165b1d55164d6a4b5ee836a6ffd6b
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 21:20:07 +0530

    Create s4.java
    create

---
+++ s4.java
@@ -0,0 +1,21 @@
+//Program to print five stars five times in the same row.
+class s4 
+{
+	public static void main(String[] args) 
+	{
+		for(int j=1;j<=5; j++)
+		{
+		for(int i=1; i<=5; i++ )
+			{
+			System.out.print("*");
+			}
+			System.out.print(" ");
+		}
+	}
+}
+/*
+
+Output
+***** ***** ***** ***** *****
+
+*/
