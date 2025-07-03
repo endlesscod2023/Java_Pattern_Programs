0a3021affd3411aedc7bb838796b92e5309b0104
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 21:16:12 +0530

    Create s2.java
    create

---
+++ s2.java
@@ -0,0 +1,22 @@
+//Program to print five stars one by one in a column.
+class s2
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=5; i++ )
+		{
+		
+		System.out.println("*");
+	}
+}
+}
+/*
+
+Output
+*
+*
+*
+*
+*
+
+*/
