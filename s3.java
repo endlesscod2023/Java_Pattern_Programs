95ca92f1176c7ab567a72fa2f5bd460c0c888484
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 21:17:26 +0530

    Create s3.java
    create

---
+++ s3.java
@@ -0,0 +1,18 @@
+//Program to print five stars in a same row.
+class s3 	
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=5; i++ )
+		{
+		
+		System.out.print("*");
+		}
+	}
+}
+/*
+
+Output
+*****
+
+*/
