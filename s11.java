3f9848c0e915a5166427c577f29ed7173ce75136
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 21:36:42 +0530

    Create s11.java
    create

---
+++ s11.java
@@ -0,0 +1,25 @@
+//Program tp print ulta(reversed) right angled triangle.
+class s11 
+{
+	public static void main(String[] args) 
+	{
+		for(int j=1;j<=5; j++)
+		{
+			for(int i=5; i>=j; i-- )
+				{
+				System.out.print("*");
+				}
+				System.out.println();
+		}
+	}
+}
+/*
+
+Output
+*****
+****
+***
+**
+*
+
+*/
