05fd05e55b1c310ef1c4dd4b4c77edd860d897e1
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-05 20:11:10 +0530

    Create s27.java
    create

---
+++ s27.java
@@ -0,0 +1,45 @@
+class s27
+{
+	public static void main(String[] args)
+		{
+		for(int i=1; i<=5; i++)
+			{
+			for(int j=1; j<=i; j++)
+				{
+				System.out.print("-");
+				}
+				for(int k=1; k<=5; k++)
+					{
+					System.out.print("*");
+					}
+					System.out.println("");
+			}
+			for(int i=1; i<=5; i++)
+				{
+				for(int j=1; j<=5-i+1; j++)
+					{
+					System.out.print("-");
+					}
+					for(int k=1; k<=5; k++)
+						{
+						System.out.print("*");
+						}
+						System.out.println("");
+				}
+		}
+}
+/*
+
+Output
+-*****
+--*****
+---*****
+----*****
+-----*****
+-----*****
+----*****
+---*****
+--*****
+-*****
+
+*/
