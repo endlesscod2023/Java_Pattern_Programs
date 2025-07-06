d83cd9a279f7d5fddb32fe4232d72cb3bb2c4c35
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-06 10:19:54 +0530

    Create s40.java
    create

---
+++ s40.java
@@ -0,0 +1,24 @@
+class s40
+{
+	public static void main(String[] args)
+		{
+		for(char i='a'; i<='e'; i++)
+			{
+			for(char j='a'; j<=i; j++)
+				{
+				System.out.print(i+" ");
+				}
+				System.out.println("");
+			}
+		}
+}
+/*
+
+Output
+a
+b b
+c c c
+d d d d
+e e e e e
+
+*/
